88862319df358be5798910b101ab9a08c92bf54b
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-07-27 16:06:43 +0530

    developer page added

--- pages/aboutus/developer/index.js
+++ pages/aboutus/developer/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import styles from '../../../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-  <h1>The News page</h1>
+  <h1>Arun Malewadi</h1>
     </div>
   )
 }
--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -4,7 +4,7 @@ import styles from '../../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-  <h1>The News page</h1>
+  <h1>About Us</h1>
     </div>
   )
 }
